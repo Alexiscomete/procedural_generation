e6863425ff2ff11b85011a664d7ad85e4939ad7b
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-10-13 20:15:00 +0200

    simple biomes

--- lib/src/main/java/procedural_generation/biomes/SimpleBiome.java
+++ lib/src/main/java/procedural_generation/biomes/SimpleBiome.java
@@ -1,5 +1,6 @@
 package procedural_generation.biomes;
 
+import procedural_generation.climat.ClimatMinMax;
 import procedural_generation.climat.ClimatRule;
 import procedural_generation.noise.ComplexNoise;
 
@@ -8,15 +9,24 @@ import java.util.HashMap;
 public class SimpleBiome implements Biome {
     private final double altitude;
     private final String name;
+    private final HashMap<ClimatRule, ClimatMinMax> climatRules;
 
-    public SimpleBiome(double altitude, String name) {
+    public SimpleBiome(double altitude, String name, HashMap<ClimatRule, ClimatMinMax> climatRules) {
         this.altitude = altitude;
         this.name = name;
+        this.climatRules = climatRules;
     }
 
     @Override
     public double pourcent(double x, double y, double altitude, HashMap<ClimatRule, Double> climatPourcent, ComplexNoise complexNoise) {
-        return 0;
+        double total = 0;
+        for (ClimatRule climatRule : climatRules.keySet()) {
+            Double pourcent = climatPourcent.get(climatRule);
+            if (pourcent != null) {
+                total += 1 - climatRules.get(climatRule).distanceRange(pourcent);
+            }
+        }
+        return total / climatRules.size();
     }
 
     @Override
