acd4772da9cca803f2bc44ecf28d4ab6edc07b84
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-12-27 15:52:54 +0100

    Starting mask

--- lib/src/test/java/procedural_generation/MaskTest.java
+++ lib/src/test/java/procedural_generation/MaskTest.java
@@ -1,9 +1,107 @@
 package procedural_generation;
 
 import org.junit.jupiter.api.Test;
+import procedural_generation.noise.ComplexNoise;
+import procedural_generation.noise.ComplexNoiseBuilder;
+import procedural_generation.noise.NoiseMapBuilder;
+import procedural_generation.noise.nodes.*;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 public class MaskTest {
     @Test
     void execute() {
+        genImage("mask");
+    }
+
+    public BufferedImage getImage() {
+        try {
+            BufferedImage image = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("DIBIMAP.png")));
+            if (image == null) {
+                throw new IOException("Image not found");
+            }
+            return image;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void genImage(String title) {
+        ComplexNoise complexNoise = new ComplexNoiseBuilder(
+                new ValueOperationNodeBuilder(
+                        new ValueOperationNodeBuilder(
+                                new AddNodeBuilder(
+                                        new ChangeLocationNodeBuilder(
+                                                new ChangeSeedNodeBuilder(
+                                                        Operation.ADD,
+                                                        1,
+                                                        new NoiseMapBuilder(1.0)
+                                                ),
+                                                Operation.DIVIDE,
+                                                Operation.DIVIDE,
+                                                10,
+                                                10
+                                        ),
+                                        new ImageMaskNodeBuilder(
+                                                getImage(),
+                                                1.0,
+                                                new Color(0x704A40),
+                                                new Color(0x4D759D)
+                                        )
+                                ),
+                                ValueOperation.POWER_SYMMETRICAL,
+                                2.0
+                        ),
+                        ValueOperation.REMOVE_POURCENT,
+                        0.4
+                )
+        ).build(60);
+
+        // generate a 2d array of noise
+        double[][] noise = new double[1000][1000];
+        for (int x = 0; x < noise.length; x++) {
+            for (int y = 0; y < noise[x].length; y++) {
+                noise[x][y] = complexNoise.getValue(x, y);
+            }
+        }
+
+        // generate an image of the noise (gray scale)
+        BufferedImage noiseImage = new BufferedImage(noise.length, noise[0].length, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < noise.length; x++) {
+            for (int y = 0; y < noise[x].length; y++) {
+                int color = (int) (noise[x][y] * 255);
+                int blue = 0;
+                int green = 0;
+                int red = 0;
+                if (color > 127) {
+                    green = 255 - color;
+                    if (color > 191) {
+                        red = 255 - color;
+                        if (color > 223) {
+                            blue = color;
+                            green = color;
+                        }
+                    }
+                } else {
+                    blue = color;
+                }
+                try {
+                    noiseImage.setRGB(x, y, (new Color(red, green, blue)).getRGB());
+                } catch (Exception e) {
+                    System.out.println("x: " + x + " y: " + y + " color: " + color);
+                }
+            }
+        }
+
+        // save the image
+        try {
+            javax.imageio.ImageIO.write(noiseImage, "png", new java.io.File(title + ".png"));
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
     }
 }
