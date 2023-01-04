b114dd93865c796fa8fb451fb11a31339e4a2cff
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2023-01-04 21:26:20 +0100

    close #10

--- lib/src/main/java/procedural_generation/noise/nodes/ImageMaskNode.java
+++ lib/src/main/java/procedural_generation/noise/nodes/ImageMaskNode.java
@@ -53,24 +53,52 @@ public class ImageMaskNode implements Node {
             return 0.5;
         }
         // case 2 : in bounds but not on a pixel (double value -> interpolation needed)
-        if (x % 1 != 0 || y % 1 != 0) {
+        if (x % 1 != 0) {
+            if (y % 1 != 0) {
+                // the pixels around the point
+                int x1 = (int) Math.floor(x);
+                int x2 = (int) Math.ceil(x);
+                int y1 = (int) Math.floor(y);
+                int y2 = (int) Math.ceil(y);
+                // the weight of the pixels
+                double x1w = x2 - x;
+                double x2w = x - x1;
+                double y1w = y2 - y;
+                double y2w = y - y1;
+                // the weighted value of the pixels
+                double x1y1w = getSimpleValue(x1, y1) * x1w * y1w;
+                double x1y2w = getSimpleValue(x1, y2) * x1w * y2w;
+                double x2y1w = getSimpleValue(x2, y1) * x2w * y1w;
+                double x2y2w = getSimpleValue(x2, y2) * x2w * y2w;
+                // the weighted sum of the pixels
+                return (x1y1w + x1y2w + x2y1w + x2y2w) / (x1w * y1w + x1w * y2w + x2w * y1w + x2w * y2w);
+            } else {
+                // the pixels around the point
+                int x1 = (int) Math.floor(x);
+                int x2 = (int) Math.ceil(x);
+                int y1 = (int) y;
+                // the weight of the pixels
+                double x1w = x2 - x;
+                double x2w = x - x1;
+                // the weighted value of the pixels
+                double x1y1w = getSimpleValue(x1, y1) * x1w;
+                double x2y1w = getSimpleValue(x2, y1) * x2w;
+                // the weighted sum of the pixels
+                return (x1y1w + x2y1w) / (x1w + x2w);
+            }
+        } else if (y % 1 != 0) {
             // the pixels around the point
-            int x1 = (int) Math.floor(x);
-            int x2 = (int) Math.ceil(x);
+            int x1 = (int) x;
             int y1 = (int) Math.floor(y);
             int y2 = (int) Math.ceil(y);
             // the weight of the pixels
-            double x1w = x2 - x;
-            double x2w = x - x1;
             double y1w = y2 - y;
             double y2w = y - y1;
             // the weighted value of the pixels
-            double x1y1w = getSimpleValue(x1, y1) * x1w * y1w;
-            double x1y2w = getSimpleValue(x1, y2) * x1w * y2w;
-            double x2y1w = getSimpleValue(x2, y1) * x2w * y1w;
-            double x2y2w = getSimpleValue(x2, y2) * x2w * y2w;
+            double x1y1w = getSimpleValue(x1, y1) * y1w;
+            double x1y2w = getSimpleValue(x1, y2) * y2w;
             // the weighted sum of the pixels
-            return (x1y1w + x1y2w + x2y1w + x2y2w) / (x1w * y1w + x1w * y2w + x2w * y1w + x2w * y2w);
+            return (x1y1w + x1y2w) / (y1w + y2w);
         }
         // case 3 : in bounds and on a pixel : 0 if near colorMin, 1 if near colorMax, 0.5 if in between
         Color color = new Color(image.getRGB((int) x, (int) y));
